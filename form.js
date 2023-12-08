bf0c20ec1e2d340686f6286711cc5a14888188f7
siddhesh-pawar <sidpawar2398@gmail.com>
2023-12-08 14:41:54 -0800

    Refactored form.js

--- form.js
+++ form.js
@@ -1,19 +1,32 @@
-function validateForm() {
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("confirmPassword").value;
-
-    if (name === "" || email === "" || password === "" || confirmPassword === "") {
-        alert("All fields are required");
-        return false;
+// This class performs client side validation
+class FormValidator {
+    constructor(name, email, password, confirmPassword) {
     }
 
-    if (password !== confirmPassword) {
-        alert("Passwords do not match");
-        return false;
-    }
+    validateForm() {
+        this.name = document.getElementById("name").value;
+        this.email = document.getElementById("email").value;
+        this.password = document.getElementById("password").value;
+        this.confirmPassword = document.getElementById("confirmPassword").value;
 
-    return true;
+        if (this.name === "" || this.email === "" || this.password === "" || this.confirmPassword === "") {
+            alert("All fields are required");
+            return false;
+        }
+
+        if (this.password !== this.confirmPassword) {
+            alert("Passwords do not match");
+            return false;
+        }
+
+        return true;
+    }
 }
-module.exports = { validateForm };
+
+const formValidator = new FormValidator();
+
+function SubmitForm() {
+    return formValidator.validateForm();
+}
+
+module.exports = { FormValidator };
