91ec995a8e4395bb4811fa37493aeb2dea90a683
aumkarringe <aumkarringe@gmail.com>
2023-12-08 16:13:15 -0800

    Apartment details added

--- details.js
+++ details.js
@@ -1,35 +1,58 @@
-document.addEventListener("DOMContentLoaded", function () {
-    function displayDetails(detailsArray) {
+class ApartmentManager {
+    constructor() {
+        this.apartmentDetails = this.retrieveDetailsFromLocalStorage() || [];
+        this.initializeListeners();
+        this.displayDetails();
+    }
+
+    retrieveDetailsFromLocalStorage() {
+        const storedDetails = localStorage.getItem("apartmentDetails");
+        return storedDetails ? JSON.parse(storedDetails) : null;
+    }
+
+    initializeListeners() {
+        document.getElementById("sortByPrice").addEventListener("change", () => this.handleFilterChange());
+        document.getElementById("filterByName").addEventListener("change", () => this.handleFilterChange());
+        document.getElementById("filterByPets").addEventListener("change", () => this.handleFilterChange());
+        document.getElementById("filterByBedrooms").addEventListener("change", () => this.handleFilterChange());
+        document.getElementById("applyFiltersBtn").addEventListener("click", () => this.handleApplyFilters());
+    }
+
+    displayDetails() {
         const detailsDiv = document.getElementById("details");
         detailsDiv.innerHTML = "<h2>All Apartment Details</h2>";
 
-        detailsArray.forEach(function (details, index) {
-            const apartmentSection = document.createElement("article");
-            apartmentSection.classList.add("apartment-section");
-
-            const apartmentDetailsDiv = document.createElement("div");
-            apartmentDetailsDiv.classList.add("apartment-details");
-
-            apartmentDetailsDiv.innerHTML = `
-                <p><strong>Apartment ${index + 1}:</strong></p>
-                <p><strong>Name:</strong> ${details.name}</p>
-                <p><strong>Location:</strong> ${details.location}</p>
-                <p><strong>Price:</strong> ${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(details.price)}</p>
-                <h2>Student Details</h2>
-                <p><strong>Student Name:</strong> ${details.studentName}</p>
-                <p><strong>Student Email:</strong> ${details.studentEmail}</p>
-                <p><strong>Student Contact:</strong> ${details.studentContact}</p>
-                <h2>Apartment Additional Information</h2>
-                <p><strong>Apartment Number:</strong> ${details.apartmentNumber}</p>
-                <p><strong>Number of Bedrooms:</strong> ${details.numBedrooms}</p>
-                <p><strong>Pets:</strong> ${details.pets}</p>
-            `;
-
-            apartmentSection.appendChild(apartmentDetailsDiv);
+        this.apartmentDetails.forEach((details, index) => {
+            const apartmentSection = this.createApartmentSection(details, index);
             detailsDiv.appendChild(apartmentSection);
         });
     }
 
+    createApartmentSection(details, index) {
+        const apartmentSection = document.createElement("article");
+        apartmentSection.classList.add("apartment-section");
+
+        const apartmentDetailsDiv = document.createElement("div");
+        apartmentDetailsDiv.classList.add("apartment-details");
+
+        apartmentDetailsDiv.innerHTML = `
+            <p><strong>Apartment ${index + 1}:</strong></p>
+            <p><strong>Name:</strong> ${details.name}</p>
+            <p><strong>Location:</strong> ${details.location}</p>
+            <p><strong>Price:</strong> ${this.formatPrice(details.price)}</p>
+            <h2>Student Details</h2>
+            <p><strong>Student Name:</strong> ${details.studentName}</p>
+            <p><strong>Student Email:</strong> ${details.studentEmail}</p>
+            <p><strong>Student Contact:</strong> ${details.studentContact}</p>
+            <h2>Apartment Additional Information</h2>
+            <p><strong>Apartment Number:</strong> ${details.apartmentNumber}</p>
+            <p><strong>Number of Bedrooms:</strong> ${details.numBedrooms}</p>
+            <p><strong>Pets:</strong> ${details.pets}</p>
+        `;
+
+        apartmentSection.appendChild(apartmentDetailsDiv);
+        return apartmentSection;
+    }
     function sortAndFilterDetails(detailsArray, sortByPrice, filterByName, filterByPets, filterByBedroomsCount) {
         // Sort by price
         if (sortByPrice === "ascending") {
